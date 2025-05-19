9a3d48ed2db0560ccbcbdcd10af910122c95acc2
Ruship7800 <ruship7800@gmail.com>
2025-05-19 06:45:47 +0000

    this is the second commit

---
+++ new.py
@@ -0,0 +1,9 @@
+# Simple Python code to greet the user
+
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Hello, {name}! Welcome to the world of Python.")
+
+#This is second commit
